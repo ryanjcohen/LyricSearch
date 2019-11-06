22ec9f2c31679cfb6d23b6c4c92ced064185202b
ryanjcohen <47368809+ryanjcohen@users.noreply.github.com>
2019-11-05 19:51:59 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,19 +1,39 @@
-//look this step up
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
-
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("searchbtn").addEventListener("click", search);
+    document.getElementById("searchbtn").addEventListener("click", search_url);
 });
 
 var text;
+var site;
 
-function search(){
+function search_url(){
 	text = document.getElementById("searchbar").value;
-	document.getElementById("p1").innerHTML= "Results for: " + text;
+	document.getElementById("searchbar").value = "";
+	document.getElementById("p1").innerHTML= "Results for: \"" + text + "\"";
+
+	var words = text.trim().split(" ");
+	var site = 'lyrics.com/lyrics/';
+
+	var i;
+	site = site+words[0];
+	for(i = 1; i<words.length; i++){
+		site = site + '%20' + words[i];
+	}
+
+	document.getElementById("p2").innerHTML = site;
+
+	const proxyurl = "https://cors-anywhere.herokuapp.com/" + site;
+	$.ajax({
+		url: proxyurl,
+		success: function(data){
+			var test = $(data).getElementsByClassName("sec-lyric clearfix");
+			console.log(test[0]);
+			//result = $(data).find("#")
+		},
+		error: function(e){
+			console.log("Error " + e);
+		}
+	});
+
 }
+
